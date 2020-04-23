0dad2629d0f336702dc8cc16aa4067cc09ca1252
Paul Gomez <pauldro952@hotmail.com>
2020-04-23 10:26:25 -0500

    Createorderdetail XML reformat

--- site/modules/Dplus/SfWebServices/xml/createorderdetail/_base.xml.php
+++ site/modules/Dplus/SfWebServices/xml/createorderdetail/_base.xml.php
@@ -2,25 +2,25 @@
 	<xs:schema id="NewDataSet" xmlns="" xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:msdata="urn:schemas-microsoft-com:xml-msdata">
 		<xs:element name="NewDataSet" msdata:IsDataSet="true" msdata:UseCurrentLocale="true">
 			<xs:complexType>
-			<xs:choice minOccurs="0" maxOccurs="unbounded">
-				<xs:element name="Result">
-					<xs:complexType>
-						<xs:sequence>
-							<xs:element name="UnitPrice" type="xs:string" minOccurs="0"/>
-							<xs:element name="ExtPrice" type="xs:string" minOccurs="0"/>
-							<xs:element name="PriceUOM" type="xs:string" minOccurs="0"/>
-							<xs:element name="UnitWeight" type="xs:string" minOccurs="0"/>
-							<xs:element name="ExtendedWeight" type="xs:string" minOccurs="0"/>
-							<xs:element name="BaseQtyUOM" type="xs:string" minOccurs="0"/>
-							<xs:element name="PackQTY" type="xs:string" minOccurs="0"/>
-							<xs:element name="PiecesPerLB" type="xs:string" minOccurs="0"/>
-							<xs:element name="AvailQTY" type="xs:string" minOccurs="0"/>
-							<xs:element name="ErrorMessage" type="xs:string" minOccurs="0"/>
-							<xs:element name="CreatedDetailLineNumber" type="xs:string" minOccurs="0"/>
-						</xs:sequence>
-					</xs:complexType>
-				</xs:element>
-			</xs:choice>
+				<xs:choice minOccurs="0" maxOccurs="unbounded">
+					<xs:element name="Result">
+						<xs:complexType>
+							<xs:sequence>
+								<xs:element name="UnitPrice" type="xs:string" minOccurs="0"/>
+								<xs:element name="ExtPrice" type="xs:string" minOccurs="0"/>
+								<xs:element name="PriceUOM" type="xs:string" minOccurs="0"/>
+								<xs:element name="UnitWeight" type="xs:string" minOccurs="0"/>
+								<xs:element name="ExtendedWeight" type="xs:string" minOccurs="0"/>
+								<xs:element name="BaseQtyUOM" type="xs:string" minOccurs="0"/>
+								<xs:element name="PackQTY" type="xs:string" minOccurs="0"/>
+								<xs:element name="PiecesPerLB" type="xs:string" minOccurs="0"/>
+								<xs:element name="AvailQTY" type="xs:string" minOccurs="0"/>
+								<xs:element name="ErrorMessage" type="xs:string" minOccurs="0"/>
+								<xs:element name="CreatedDetailLineNumber" type="xs:string" minOccurs="0"/>
+							</xs:sequence>
+						</xs:complexType>
+					</xs:element>
+				</xs:choice>
 			</xs:complexType>
 		</xs:element>
 	</xs:schema>
@@ -30,7 +30,7 @@
 				<?php if ($error) : ?>
 					<ErrorMessage><?= $message; ?></ErrorMessage>
 				<?php else : ?>
-					<?= $orderdetail; ?>
+					<?= $xml_orderdetail; ?>
 				<?php endif; ?>
 			</Result>
 		</NewDataSet>
--- site/modules/Dplus/SfWebServices/xml/createorderdetail/orderdetail.xml.php
+++ site/modules/Dplus/SfWebServices/xml/createorderdetail/orderdetail.xml.php
@@ -1,11 +1,11 @@
-<UnitPrice></UnitPrice>
-<ExtPrice></ExtPrice>
-<PriceUOM></PriceUOM>
-<UnitWeight></UnitWeight>
-<ExtendedWeight></ExtendedWeight>
-<BaseQtyUOM></BaseQtyUOM>
-<PackQTY></PackQTY>
-<PiecesPerLB></PiecesPerLB>
-<AvailQTY></AvailQTY>
+<UnitPrice><?= $orderdetail['UnitPrice']; ?></UnitPrice>
+<ExtPrice><?= $orderdetail['ExtPrice']; ?></ExtPrice>
+<PriceUOM><?= $orderdetail['PriceUOM']; ?></PriceUOM>
+<UnitWeight><?= $orderdetail['UnitWeight']; ?></UnitWeight>
+<ExtendedWeight><?= $orderdetail['ExtendedWeight']; ?></ExtendedWeight>
+<BaseQtyUOM><?= $orderdetail['BaseQtyUOM']; ?></BaseQtyUOM>
+<PackQTY><?= $orderdetail['PackQTY']; ?></PackQTY>
+<PiecesPerLB><?= $orderdetail['PiecesPerLB']; ?></PiecesPerLB>
+<AvailQTY><?= $orderdetail['AvailQTY']; ?></AvailQTY>
 <ErrorMessage></ErrorMessage>
-<CreatedDetailLineNumber></CreatedDetailLineNumber>
+<CreatedDetailLineNumber><?= $orderdetail['CreatedDetailLineNumber']; ?></CreatedDetailLineNumber>
