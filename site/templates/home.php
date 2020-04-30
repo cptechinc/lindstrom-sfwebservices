8cf3a783ecb833c7a9fbac06a4079811c32e3fe9
Paul Gomez <pauldro952@hotmail.com>
2020-04-30 13:43:22 -0500

    Update home.php

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,10 +1,5 @@
-<?php $session->redirect($pages->get('/tests/')->url); ?>
-
-<?php include('./_head.php'); ?>
-	<div class='container page pt-3'>
-		<?php if ($page->show_title) : ?>
-			<h2><?= $page->get('headline|title'); ?></h2>
-		<?php endif; ?>
-		<?= $page->body; ?>
-	</div>
-<?php include('./_foot.php'); ?>
+<?php
+	$html = $modules->get('HtmlWriter');
+	$page->body = $html->h2('', 'Test API');
+	$page->body .= render_php("{$config->paths->templates}inc/menu/list.php", $args = array('items' => $pages->get('template=api, name=tests')->children()));
+	include __DIR__ . "/basic-page.php";
