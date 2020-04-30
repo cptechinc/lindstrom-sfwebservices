3490b85d21f2f4bd79b74e8cb1130cf60f0ad8da
Paul Gomez <pauldro952@hotmail.com>
2020-04-30 13:43:06 -0500

    pages

--- site/templates/basic-page.php
+++ site/templates/basic-page.php
@@ -1,6 +1,8 @@
 <?php include('./_head.php'); ?>
+	<div class="jumbotron jumbotron-pagetitle">
+		<h1 class="display-4"><?= $page->get('headline|title'); ?></h1>
+	</div>
 	<div class='container page pt-3'>
-		<h2><?= $page->get('headline|title'); ?></h2>
 		<?= $page->body; ?>
 	</div>
 <?php include('./_foot.php'); ?>
