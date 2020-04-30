b5da10d4572aa4b31ec5c94369554c5569360cd3
Paul Gomez <pauldro952@hotmail.com>
2020-04-30 13:42:59 -0500

    Delete create-quote.php

--- site/templates/create-quote.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$endpoint = $modules->get('CreateQuote');
-	$endpoint->url = $page->url;
-
-	echo $pages->get("template=service,service=$page->service")->url;
-
-	//$page->body .= render_php("{$config->paths->templates}inc/quote/form.php", $args = array('endpoint' => $endpoint));
-
-	//include __DIR__ . "/basic-page.php";
