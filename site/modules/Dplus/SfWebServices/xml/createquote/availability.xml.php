840549257783a5a8667bfde54673f78efafd937a
Paul Gomez <pauldro952@hotmail.com>
2020-07-29 13:52:12 -0500

    Update availability.xml.php

--- site/modules/Dplus/SfWebServices/xml/createquote/availability.xml.php
+++ site/modules/Dplus/SfWebServices/xml/createquote/availability.xml.php
@@ -2,6 +2,6 @@
 	<AvailQTY diffgr:id="AvailQTY<?= $index; ?>" msdata:rowOrder="<?= $index - 1; ?>">
 		<WHID><?= $warehouse['WHID']; ?></WHID>
 		<QTY><?= $warehouse['QTY']; ?></QTY>
-		<QTY_PACKAGE_MAX><?= $warehouse['_PACKAGE_MAX'] + 0; ?></QTY_PACKAGE_MAX>
+		<QTY_PACKAGE_MAX><?= $warehouse['QTY_PACKAGE_MAX'] + 0; ?></QTY_PACKAGE_MAX>
 	</AvailQTY>
 <?php endforeach; ?>
