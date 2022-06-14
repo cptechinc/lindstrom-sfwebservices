4d32b2ee5d0adacdb4f3a406cb73251f050f1c95
Paul <pauldro952@outlook.com>
2022-06-14 10:21:34 -0500

    FIX: add pagination to the log

---
+++ site/templates/inc/util/paginator.php
@@ -0,0 +1,46 @@
+<?php
+	foreach ($args as $variable => $value) {
+		$$variable = $value;
+	}
+
+	$totalPages = ceil($totalLogEntries / 100);
+?>
+
+<div class="d-flex w-100 justify-content-between paginator">
+	<div></div>
+	<div>
+		<ul class="pagination">
+			<li class="page-item <?= ($input->pageNum == 1) ? 'disabled' : '' ?>">
+				<a href="<?= $input->paginateUrl($input->pageNum - 1); ?>" class="page-link paginator-link" aria-label="Previous">
+					<span aria-hidden="true">&laquo;</span>
+				</a>
+			</li>
+			<li class="page-item <?= ($input->pageNum == i) ? 'active' : '' ?>">
+				<div class="dropdown">
+					<a class="page-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Page <?= $input->pageNum; ?> of <?= $totalPages; ?>
+					</a>
+
+					<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+						<?php for ($i = 1; $i < $totalPages; $i++) : ?>
+							<?php if ($i > 0) : ?>
+								<a class="dropdown-item paginator-link <?= ($input->pageNum == $i) ? 'active' : '' ?>" href="<?= $input->paginateUrl($i) ?>">
+									<?= $i ?>
+								</a>
+							<?php endif; ?>
+						<?php endfor; ?>
+					</div>
+				</div>
+			</li>
+
+			<li class="page-item <?= ($input->pageNum == $totalPages) ? 'disabled' : '' ?>">
+				<a href="<?= $input->paginateUrl($input->pageNum + 1); ?>" aria-label="Next" class="page-link paginator-link">
+					<span aria-hidden="true">&raquo;</span>
+				</a>
+			</li>
+		</ul>
+	</div>
+	<div>
+
+	</div>
+</div>
--- site/templates/log.php
+++ site/templates/log.php
@@ -1,4 +1,5 @@
 <?php
 $lines = $log->getLines($page->name, $options = []);
 $page->body .= render_php("{$config->paths->templates}inc/logs/$page->name/page.php", $args = array('page' => $page, 'lines' => $lines));
+$page->body .= render_php("{$config->paths->templates}inc/util/paginator.php", $args = array('page' => $page, 'input' => $input, 'totalLogEntries' => $log->getTotalEntries($page->name)));
 include __DIR__ . "/basic-page.php";
