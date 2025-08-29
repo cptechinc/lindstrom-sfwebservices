ac0efb5d00bd85eee644352e37983a00c7397e1d
Paul Gomez <pauldro952@outlook.com>
2025-08-29 11:15:31 -0500

    REFACTOR: use log util

--- site/templates/log.php
+++ site/templates/log.php
@@ -4,10 +4,17 @@ use ProcessWire\Page;
 use ProcessWire\WireHttp;
 use ProcessWire\WireInput;
 use ProcessWire\WireLog;
+use SfWebServices\Log\LogUtil;
+
+
 
 /** @var Page $page */
 /** @var WireInput $input */
 /** @var WireLog $log */
+
+
+$logUtil = new LogUtil();
+$logUtil->logname = $page->name;
 
 $get = $input->get;
 
@@ -15,23 +22,11 @@ if ($get->offsetExists('prune')) {
     $log->prune($page->name, 365);
 }
 
-$options = [];
-
-if ($input->get->offsetExists('filter')) {
-    $options = [
-        'dateFrom' => $get->offsetExists('fromDate') ? strtotime($get->text('fromDate')) : 0,
-        'dateTo'   => $get->offsetExists('toDate') ? strtotime($get->text('toDate')) : time(),
-    ];
-}
-
-$lines = $log->getLines($page->name, $options);
-
 if ($get->offsetExists('download')) {
-    $options['limit'] = 0;
-    $lines = $log->getLines($page->name, $options);
+    $lines = $logUtil->fetchAllLines();
     $dir = $files->tempDir('download');
     $newFile = $dir->get() . $page->name . '.txt';
-    $content = implode("\n", array_values($lines));
+    $content = implode("\n", array_values($lines->getArray()));
     file_put_contents($newFile, $content);
 
     $http = new WireHttp();
@@ -39,15 +34,8 @@ if ($get->offsetExists('download')) {
     exit;
 }
 
-$totalLogEntries = 0;
-
-if (count($lines) > 0) {
-    $firstKeys = explode('/', array_keys($lines)[0]);
-
-    $totalLogEntries = $firstKeys[1];
-}
-
+$lines = $logUtil->fetchLines();
 
 $page->body .= render_php("{$config->paths->templates}inc/logs/$page->name/page.php", ['page' => $page, 'lines' => $lines, 'input' => $input, 'config' => $config]);
-$page->body .= render_php("{$config->paths->templates}inc/util/paginator.php", ['page' => $page, 'input' => $input, 'totalLogEntries' => $totalLogEntries]);
+$page->body .= render_php("{$config->paths->templates}inc/util/paginator.php", ['page' => $page, 'input' => $input, 'totalLogEntries' => $lines->data('totalCount')]);
 include __DIR__ . "/basic-page.php";
