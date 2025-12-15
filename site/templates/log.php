e10927f610dcc9851e937a8545a3eddbeb84b7ac
Paul Gomez <pauldro952@outlook.com>
2025-12-15 15:15:01 -0600

    Update log.php

--- site/templates/log.php
+++ site/templates/log.php
@@ -5,6 +5,9 @@ use ProcessWire\WireHttp;
 use ProcessWire\WireInput;
 use ProcessWire\WireLog;
 use SfWebServices\Log\LogUtil;
+use SfWebServices\Log\LoginLog;
+use SfWebServices\Log\RequestLog;
+use SfWebServices\Log\SessionLog;
 
 
 
@@ -13,29 +16,36 @@ use SfWebServices\Log\LogUtil;
 /** @var WireLog $log */
 
 
-$logUtil = new LogUtil();
-$logUtil->logname = $page->name;
+$dpLog = new SessionLog();
+
+if ($page->name == 'login') {
+	$dpLog = new LoginLog();
+}
+
+if ($page->name == 'request') {
+	$dpLog = new RequestLog();
+}
 
 $get = $input->get;
 
 if ($get->offsetExists('prune')) {
-    $log->prune($page->name, 365);
+	$log->prune($page->name, 365);
 }
 
 if ($get->offsetExists('download')) {
-    $lines = $logUtil->fetchAllLines();
-    $dir = $files->tempDir('download');
-    $newFile = $dir->get() . $page->name . '.txt';
-    $content = implode("\n", array_values($lines->getArray()));
-    file_put_contents($newFile, $content);
+	$lines = $dpLog->logutil->fetchAllLines();
+	$dir = $files->tempDir('download');
+	$newFile = $dir->get() . $page->name . '.txt';
+	$content = implode("\n", array_values($lines->getArray()));
+	file_put_contents($newFile, $content);
 
-    $http = new WireHttp();
-    $http->sendFile($newFile, ['forceDownload' => true]);
-    exit;
+	$http = new WireHttp();
+	$http->sendFile($newFile, ['forceDownload' => true]);
+	exit;
 }
 
-$lines = $logUtil->fetchLines();
+$lines = $dpLog->logutil->fetchLines();
 
-$page->body .= render_php("{$config->paths->templates}inc/logs/$page->name/page.php", ['page' => $page, 'lines' => $lines, 'input' => $input, 'config' => $config]);
+$page->body .= render_php("{$config->paths->templates}inc/logs/$page->name/page.php", ['page' => $page, 'lines' => $lines, 'input' => $input, 'config' => $config, 'LOG' => $dpLog]);
 $page->body .= render_php("{$config->paths->templates}inc/util/paginator.php", ['page' => $page, 'input' => $input, 'totalLogEntries' => $lines->data('totalCount')]);
 include __DIR__ . "/basic-page.php";
